487bc574bb6fe07eb24e53882021c507f8c8b72f
Brian Abeyta-Pratt <babeytapratt@gmail.com>
2020-10-08 18:41:42 -0600

    cannot get api working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import { BASE_URL, API_KEY } from './constants/index'
 import "./App.css";
-import './constants/index'
 import Image from './image'
+import axios from 'axios'
 
 
 function App() {
+
+  // const { camera, close } = props
+  const [imageData, setImageData] = useState('')
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+    .then(res => {
+        setImageData(res.data)})
+    .catch(err => {
+
+    })
+    console.log(imageData)
+  },)
+  console.log(imageData)
+
   return (
     <div className="App">
       <h1>NASA Photo of the Day</h1>
-        <Image/>
-
+      <Image/>
     </div>
-
-
   );
+
 }
 
 export default App;
--- src/image.js
+++ src/image.js
@@ -5,27 +5,9 @@ import axios from "axios";
 import { API_KEY, BASE_URL } from "./constants/index";
 
 export default function Image(props) {
-    const { camera, close } = props
-    const [imageData, setImageData] = useState([])
+    const [imageData, setImageData] = useState('')
 
-    function randomImage(data) {
-        return data[Math.floor(Math.random() *(data.length + 1))]
-    }
-
-
-    useEffect(() => {
-        axios.get(`${BASE_URL}${camera}/photos?earth_date=2015-10-6&api_key=${API_KEY}`)
-        .then(res => {
-            setImageData(randomImage(res.data))
-        })
-        .catch(err => {
-
-        })
-        console.log(imageData)
-    });
-    console.log(imageData)
-
-return (
+    return (
     <div className="image">
         <img src={imageData.img_src} alt="NASA_image"></img>
 
